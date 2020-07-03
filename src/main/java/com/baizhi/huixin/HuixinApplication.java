1faeab564417b29b5cac985f13b976fc5d286127
rjg <18130963290@123.com>
2020-07-03 21:54:55 +0800

    这是我的第一次提交

---
+++ src/main/java/com/baizhi/huixin/HuixinApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.huixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuixinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuixinApplication.class, args);
+    }
+
+}
